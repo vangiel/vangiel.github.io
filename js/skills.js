854845d4a26c252ada3e4f4342ec3de95a900b0a
Daniel Rodriguez <dani1610@hotmail.com>
2020-08-27 13:19:23 +0200

    Some small fixes

--- js/skills.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Daniel on 30/01/2017.
- */
-
-var valueSkill = document.getElementsByClassName("skill__porcentage");
-
-for(var i=0; i<valueSkill.length; i++){
-     valueSkill[i].firstElementChild.style.width= valueSkill[i].firstElementChild.innerHTML;
-}
