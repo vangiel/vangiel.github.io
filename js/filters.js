602688ed798800da7a4e00afc6a4ade90d76f8c1
Daniel Rodriguez <dani1610@hotmail.com>
2023-12-17 13:00:42 +0100

    Fixed menu buttons

--- js/filters.js
+++ js/filters.js
@@ -1,4 +1,3 @@
-
 /* Projects searchaer */
 
 function myFilter() {
@@ -8,15 +7,15 @@ function myFilter() {
     filter = input.value.toUpperCase();
     ul = document.getElementById("menu");
     li = ul.getElementsByTagName('li');
-  
+
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
-  }
+}
